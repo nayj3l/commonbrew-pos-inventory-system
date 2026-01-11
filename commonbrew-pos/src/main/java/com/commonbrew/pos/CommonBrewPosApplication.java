2111a05c9c7b707d2bb40271d1cedc51b89e36ba
nayj3l <73902291+nayj3l@users.noreply.github.com>
2026-01-11 19:58:39 +0800

    Initial Commit

---
+++ commonbrew-pos/src/main/java/com/commonbrew/pos/CommonBrewPosApplication.java
@@ -0,0 +1,13 @@
+package com.commonbrew.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonBrewPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonBrewPosApplication.class, args);
+	}
+
+}
---
+++ commonbrew-pos/src/test/java/com/commonbrew/pos/CommonBrewPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commonbrew.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonBrewPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
